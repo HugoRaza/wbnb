f8d42abbd6eb6cd34a78edd7283b819cc49b5eb5
Leon Boutrand <leonboutrand@gmail.com>
2019-02-20 15:48:14 +0100

    end rental date must be higher than start date

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -4,6 +4,7 @@ class Rental < ApplicationRecord
   has_one :vehicle_review, dependent: :destroy
 
   validate :custom_rule
+  validate :date_rule
   validates :status, inclusion: { in: %w[pending accepted declined] }
 
   private
@@ -11,5 +12,9 @@ class Rental < ApplicationRecord
   # the client is not renting his own vehicle
   def custom_rule
     errors.add(:user_id, "You cannot rent your own vehicle") if user_id == vehicle.user_id
+  end
+
+  def date_rule
+    errors.add(:end_date, "End date must be greater than start day") if end_date < start_date
   end
 end
