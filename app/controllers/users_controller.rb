283fae5eb34e24437add015ec0643c5fcf06786f
hugo razafindranaly <hugo.raza@gmail.com>
2019-02-20 17:32:51 +0100

    display profile and edit information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,29 @@
 class UsersController < ApplicationController
-  before_action :set_user
+  before_action :set_user, only: [:owner_dashboard, :my_profile, :edit, :update]
 
   def owner_dashboard
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def my_profile
+    render :show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.email = params[:user][:email]
+    @user.first_name = params[:user][:first_name]
+    @user.last_name = params[:user][:last_name]
+    @user.profile_picture = params[:user][:profile_picture]
+    @user.save
+    redirect_to my_profile_path
+  end
+
 
   private
 
