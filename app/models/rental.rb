b140075d6fc2070d5e88daeaf97bbac01b3321db
Leon Boutrand <leonboutrand@gmail.com>
2019-02-20 15:57:05 +0100

    method naming

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,14 +3,14 @@ class Rental < ApplicationRecord
   belongs_to :vehicle
   has_one :vehicle_review, dependent: :destroy
 
-  validate :custom_rule
+  validate :user_rental_rule
   validate :date_rule
   validates :status, inclusion: { in: %w[pending accepted declined] }
 
   private
 
   # the client is not renting his own vehicle
-  def custom_rule
+  def user_rental_rule
     errors.add(:user_id, "You cannot rent your own vehicle") if user_id == vehicle.user_id
   end
 
