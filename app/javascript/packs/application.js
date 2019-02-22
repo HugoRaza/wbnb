22b93c9eb2cb00f49421619bdd179d92df914624
Leon Boutrand <leonboutrand@gmail.com>
2019-02-22 16:47:57 +0100

    js moved

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,3 +6,18 @@ initAutocomplete();
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import initMapbox from '../plugins/init_mapbox';
 initMapbox();
+
+const ongoing = document.querySelector("#reservations-ongoing");
+const past = document.querySelector("#reservations-declined");
+const tabs = document.querySelectorAll(".tab");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (event) => {
+    ongoing.classList.toggle("hidden");
+    past.classList.toggle("hidden");
+    tabs.forEach((subtab) => {
+      subtab.classList.toggle("disabled");
+      subtab.classList.toggle("active");
+    });
+  });
+});
